fcb3f8596318f7fcdd9dc952cee16d805618f32a
ZOU JIA CHING <jctzou@gmail.com>
2015-03-10 15:39:28 +0800

    first buildgit

---
+++ ksana.js
@@ -0,0 +1,48 @@
+jsonp_handler({
+ "version": "1",
+ "build": 2,
+ "title": "myapp",
+ "minruntime": 1,
+ "baseurl": "http://rawgit.com/jctzou/myapp/master/",
+ "description": "",
+ "browserify": {
+  "external": [
+   "react",
+   "bootstrap"
+  ]
+ },
+ "files": [
+  "index.html",
+  "index.css",
+  "react-bundle.js",
+  "bundle.js",
+  "jquery.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css",
+  "sample.kdb"
+ ],
+ "filesizes": [
+  547,
+  64,
+  652507,
+  146050,
+  277980,
+  381,
+  35607,
+  113502,
+  97542
+ ],
+ "filedates": [
+  "2015-03-10T07:37:31.263Z",
+  "2015-03-10T07:37:31.259Z",
+  "2015-03-10T07:37:31.355Z",
+  "2015-03-10T07:37:37.732Z",
+  "2015-03-10T07:37:31.305Z",
+  "2015-03-10T07:37:31.308Z",
+  "2015-03-10T07:37:31.252Z",
+  "2015-03-10T07:37:31.208Z",
+  "2015-03-10T07:37:34.870Z"
+ ],
+ "date": "2015-03-10T07:37:38.692Z"
+})
